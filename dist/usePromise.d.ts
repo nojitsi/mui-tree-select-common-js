7f5c8d9986469927197bfdf7a473b629962b79d1
yehoriliushchenko <yehor.iliuschenko@gen.tech>
2022-11-20 08:35:47 +0200

    deleted dist, who added it?

--- dist/usePromise.d.ts
+++
@@ -1,8 +0,0 @@
-declare const usePromise: <Result = unknown, ErrorType = Error>(
-  promise: Result | Promise<Result>
-) => {
-  loading: boolean;
-  error: ErrorType | null;
-  data: Result | undefined;
-};
-export default usePromise;
